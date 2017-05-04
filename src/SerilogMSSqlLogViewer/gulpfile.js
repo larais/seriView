bd3a3644592abf997754298d039823c4bc071ce3
Sebastian Lang <admin@sebastian-lang.eu>
2017-05-04 22:24:45 +0200

    Copy relevant library files via gulp, remove files of bower libraries

--- src/SerilogMSSqlLogViewer/gulpfile.js
+++ src/SerilogMSSqlLogViewer/gulpfile.js
@@ -1,10 +1,14 @@
-﻿/// <binding BeforeBuild='default' />
+﻿/// <binding BeforeBuild='default' ProjectOpened='copy-libs:release, copy-libs:dev' />
 
 var gulp = require('gulp'),
     ts = require("gulp-typescript"),
     gulpTypings = require("gulp-typings"),
     rimraf = require("rimraf");
 
+var paths = {
+    webroot: "./wwwroot/",
+    bowerroot: "./bower_components/"
+};
 
 gulp.task('clean', function () {
     return gulp.src('./wwwroot/**/*.js', { read: false })
@@ -24,6 +28,29 @@ gulp.task('compile-ts', function () {
             out: 'site.js'
         }))
         .pipe(gulp.dest('./wwwroot/js'));
+});
+
+gulp.task("copy-semantic-ui", function () {
+    return gulp.src([
+        paths.bowerroot + "semantic/dist/**/**"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib/semantic"));
+});
+
+gulp.task('copy-libs:release', ["copy-semantic-ui"], function () {
+    return gulp.src([
+        paths.bowerroot + "jquery/dist/jquery.min.js",
+        paths.bowerroot + "jsrender/jsrender.min.js"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib"));
+});
+
+gulp.task('copy-libs:dev', ["copy-semantic-ui"], function () {
+    return gulp.src([
+        paths.bowerroot + "jquery/dist/jquery.js",
+        paths.bowerroot + "jsrender/jsrender.js"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib"));
 });
 
 gulp.task('default', ['compile-ts']);
