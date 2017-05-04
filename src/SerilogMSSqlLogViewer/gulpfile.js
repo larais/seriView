c1a6295d649882049138bbe83fbeca48b9a1d1a5
David Morais Ferreira <david.moraisferreira@gmail.com>
2017-05-04 22:28:07 +0200

    Added Gulp, TypeScript & Typings (#1)
    * Added Gulp, TypeScript & Typings
    
    * Copy relevant library files via gulp, remove files of bower libraries

---
+++ src/SerilogMSSqlLogViewer/gulpfile.js
@@ -0,0 +1,56 @@
+﻿/// <binding BeforeBuild='default' ProjectOpened='copy-libs:release, copy-libs:dev' />
+
+var gulp = require('gulp'),
+    ts = require("gulp-typescript"),
+    gulpTypings = require("gulp-typings"),
+    rimraf = require("rimraf");
+
+var paths = {
+    webroot: "./wwwroot/",
+    bowerroot: "./bower_components/"
+};
+
+gulp.task('clean', function () {
+    return gulp.src('./wwwroot/**/*.js', { read: false })
+        .pipe(rimraf());
+});
+
+gulp.task("installTypings", function () {
+    var stream = gulp.src("./typings.json")
+        .pipe(gulpTypings()); 
+    return stream;
+});
+
+gulp.task('compile-ts', function () {
+    gulp.src('./TypeScript/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            out: 'site.js'
+        }))
+        .pipe(gulp.dest('./wwwroot/js'));
+});
+
+gulp.task("copy-semantic-ui", function () {
+    return gulp.src([
+        paths.bowerroot + "semantic/dist/**/**"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib/semantic"));
+});
+
+gulp.task('copy-libs:release', ["copy-semantic-ui"], function () {
+    return gulp.src([
+        paths.bowerroot + "jquery/dist/jquery.min.js",
+        paths.bowerroot + "jsrender/jsrender.min.js"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib"));
+});
+
+gulp.task('copy-libs:dev', ["copy-semantic-ui"], function () {
+    return gulp.src([
+        paths.bowerroot + "jquery/dist/jquery.js",
+        paths.bowerroot + "jsrender/jsrender.js"
+    ])
+        .pipe(gulp.dest("./wwwroot/lib"));
+});
+
+gulp.task('default', ['compile-ts']);
