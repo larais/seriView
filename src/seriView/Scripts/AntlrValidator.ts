352e732d3763a557a2be00249ec5e243b4fc2cf0
Sebastian Lang <admin@sebastian-lang.eu>
2017-12-02 23:40:00 +0100

    Add validator functions + integrate into filter UI

---
+++ src/seriView/Scripts/AntlrValidator.ts
@@ -0,0 +1,32 @@
+﻿declare var require: any;
+
+var antlr4 = require('antlr4/index');
+var SQELexer = require('lib/SQE/SQELexer');
+var SQEParser = require('lib/SQE/SQEParser');
+
+class AntlrValidator {
+    private errorListener: AntlrErrorListener = new AntlrErrorListener();
+
+
+    isValid(input: string): boolean {
+        this.errorListener.reset();
+
+        var chars = new antlr4.InputStream(input);
+        var lexer = new SQELexer.SQELexer(chars);
+
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(this.errorListener);
+
+        var tokens = new antlr4.CommonTokenStream(lexer);
+        var parser = new SQEParser.SQEParser(tokens);
+
+        parser.removeErrorListeners();
+        parser.addErrorListener(this.errorListener);
+
+        var tree = parser.expression();
+
+        console.debug("Parsed: " + tree.toStringTree());
+
+        return !this.errorListener.hasError;
+    }
+}
--- src/seriView/Scripts/Components/LogFilter.ts
+++ src/seriView/Scripts/Components/LogFilter.ts
@@ -1,32 +1,27 @@
 ﻿import Vue from 'vue'
 
-declare var require: any;
-
-var antlr4 = require('antlr4/index');
-var SQELexer = require('lib/SQE/SQELexer');
-var SQEParser = require('lib/SQE/SQEParser');
+var antlrValidator = new AntlrValidator();
 
 Vue.component("log-filter", {
     template: "#logFilterTmpl",
     data: function () {
         return {
-            filter: ""
+            filter: "",
+            is_valid: false
         };
     },
+
+    watch: {
+        filter: function (newValue) {
+            this.is_valid = antlrValidator.isValid(this.filter);
+        }
+    },
+
     methods: {
         evClickApply: function () {
-
-            var chars = new antlr4.InputStream(this.filter);
-            var lexer = new SQELexer.SQELexer(chars);
-
-            var tokens = new antlr4.CommonTokenStream(lexer);
-            var parser = new SQEParser.SQEParser(tokens);
-
-            var tree = parser.expression();
-
-            console.log("Parsed: " + tree.toStringTree());
-
-            this.$emit("em_apply_filter", this.filter);
+            if (this.is_valid) {
+                this.$emit("em_apply_filter", this.filter);
+            }
         }
     }
 });
